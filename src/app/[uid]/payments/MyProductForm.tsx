a06f50d4e0c7f0a2692481eea816fc0a9c989316
kunghwan <ysw03031@naver.com>
2025-04-15 16:21:58 +0900

    init

--- src/app/[uid]/payments/MyProductForm.tsx
+++ src/app/[uid]/payments/MyProductForm.tsx
@@ -4,7 +4,7 @@ import { isNum } from "@/utils";
 import { ChangeEvent, useCallback, useMemo, useState } from "react";
 
 interface Props {
-  payload?: Product; //! payload가 있으면 수정 : UPDATE 없으면 추가: CREATE
+  payload?: Product; //! payload 가 있으면 수정 : UPDATE 없으면 추가: CREATE
   onSubmit: (product: Product) => void;
 }
 
@@ -13,7 +13,7 @@ const MyProductForm = ({ onSubmit, payload }: Props) => {
   const initialState = useMemo<Product>(
     () =>
       payload ?? {
-        created_at: new Date().toDateString(),
+        created_at: new Date().toLocaleString(),
         description: [],
         id: "",
         imgUrls: [],
@@ -23,10 +23,9 @@ const MyProductForm = ({ onSubmit, payload }: Props) => {
       },
     [payload, user]
   );
-  const [product, setProduct] = useState(initialState);
 
+  const [product, setProduct] = useState(initialState);
   const [desc, setDesc] = useState("");
-  const [isDescFocused, setIsDescFocused] = useState(false);
 
   const Name = useTextInput();
   const Desc = useTextInput();
@@ -38,7 +37,7 @@ const MyProductForm = ({ onSubmit, payload }: Props) => {
       setProduct((prev) => ({
         ...prev,
         [event.target.name]:
-          event.target.value === "price" ? Number(value) : value,
+          event.target.name === "price" ? Number(value) : value,
       }));
     },
     []
@@ -46,43 +45,40 @@ const MyProductForm = ({ onSubmit, payload }: Props) => {
 
   const nameMessage = useMemo(() => {
     if (product.name.length === 0) {
-      return "상품명을 입력";
+      return "상품명을 입력해주세요.";
     }
     return null;
   }, [product.name]);
-
   const descMessage = useMemo(() => {
     if (desc.length === 0) {
-      return "상품명을 입력";
+      return "상품상세설명을 입력해주세요.";
     }
     return null;
   }, [desc]);
-
   const descsMessage = useMemo(() => {
     if (product.description.length === 0) {
-      return "상품상세설명을 1줄 이상 입력해주세오ㅛ";
+      return "상품상세설명을 1줄 이상 입력해주세요.";
     }
     return null;
   }, [product.description]);
-
   const priceMessage = useMemo(() => {
-    console.log(typeof product.price);
+    console.log(typeof product.price, product.price);
     if (!isNum(product.price)) {
-      return "숫자만 입력해주세요";
+      return "숫자만 입력해주세요.";
     }
     if (product.price === 0) {
-      return "상품가격을 확인해주세요";
+      return "상품가격을 확인해주세요.";
     }
-
     return null;
   }, [product.price]);
+
   return (
     <Form>
       <Name.TextInput
         value={product.name}
         onChangeText={onChangeP}
         name="name"
-        label="상풍명"
+        label="상품명"
         placeholder="피카츄 모자"
         message={nameMessage}
       />
@@ -91,15 +87,15 @@ const MyProductForm = ({ onSubmit, payload }: Props) => {
         onChangeText={setDesc}
         name="description"
         label="상품 상세 설명"
-        placeholder="피카츄 모자의 디테일"
+        placeholder="피카츄 모자의 디테일 설명하기"
         message={descMessage}
       />
       <Price.TextInput
-        value={product.price.toString()} //!number 타입을 문자열로 바꿔서 전달해주면 처음에 시작하는 0을 없앨 수 있음
+        value={product.price.toString()} //! number 타입을 문자열로 바꿔서 전달해주면 처음에 시작하는 0을 없앨 수 있음.
         onChangeText={onChangeP}
         name="price"
-        label="상품 가격"
-        placeholder="10,0000"
+        label="상품가격"
+        placeholder="100,000"
         type="number"
         message={priceMessage}
       />
